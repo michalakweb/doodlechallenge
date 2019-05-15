ee8d88d45d31402b945bfaa87f66ddad6aa3a424
Mateusz Michalak <michalakweb@gmail.com>
2019-05-15 13:01:31 +0200

    Updates

---
+++ precache-manifest.c00aec33ef47b910269d254c4231ca2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4a6b681773b2d85651be296083afba",
+    "url": "/doodlechallenge/index.html"
+  },
+  {
+    "revision": "e6d7af62da9c8da4ecdc",
+    "url": "/doodlechallenge/static/css/main.58f4c94c.chunk.css"
+  },
+  {
+    "revision": "7685573bbbdc0ba85f9a",
+    "url": "/doodlechallenge/static/js/2.2a3879e5.chunk.js"
+  },
+  {
+    "revision": "e6d7af62da9c8da4ecdc",
+    "url": "/doodlechallenge/static/js/main.4c8cf6d5.chunk.js"
+  },
+  {
+    "revision": "eac17e5fc703fa7328c1",
+    "url": "/doodlechallenge/static/js/runtime~main.c83bc28e.js"
+  },
+  {
+    "revision": "d0aa956a15233378e370c9fc0b302b68",
+    "url": "/doodlechallenge/static/media/doodles.d0aa956a.png"
+  }
+]);
