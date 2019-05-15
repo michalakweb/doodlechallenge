77893ec21307b10cb6aff03a19bb4974d124b6aa
Mateusz Michalak <michalakweb@gmail.com>
2019-05-15 10:38:15 +0200

    added state and list of components to add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    messages: []
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        Hello
+        {
+          //Navbar
+          //MessageList
+          //SendMessageForm
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
